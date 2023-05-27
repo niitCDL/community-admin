632166394ffcb61a3a1c1399bc1d5a41cc6a8e39
haohao86ys <1943138987@qq.com>
2023-05-28 00:17:16 +0800

    feat(custom): 初步：账单管理

---
+++ src/api/society/order.ts
@@ -0,0 +1,7 @@
+﻿import service from '@/utils/request'
+import cache from '@/utils/cache'
+import constant from '@/utils/constant'
+
+export const useUserExportApi = () => {
+	location.href = constant.apiUrl + '/soft2242/order/export?accessToken=' + cache.getToken()
+}
