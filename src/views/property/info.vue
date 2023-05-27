94e76026ec319422d85870e7f0c32eef8f9977cb
xl123-ss <3179716692@qq.com>
2023-05-27 13:46:50 +0800

    feat(custom): 公告管理的页面完善

---
+++ src/views/property/info.vue
@@ -0,0 +1,21 @@
+<template>
+{{ data }}	
+</template>
+
+<script setup lang="ts">
+	
+	let data = localStorage.getItem("info")
+	data = JSON.parse(data)
+	
+</script>
+
+<style scoped>
+.layout-home {
+	line-height: 2;
+	font-size: 15px;
+}
+
+.layout-home h2 {
+	margin: 6px 0;
+}
+</style>
