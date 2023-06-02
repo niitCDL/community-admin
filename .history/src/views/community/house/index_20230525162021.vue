f1dea9138a5aab650b75b3355620694da422ded4
OP <1332869575@qq.com>
2023-06-02 16:07:48 +0800

    feat(custom): 车辆车位导入导出+ts数据类型修复

--- .history/src/views/community/house/index_20230525162021.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-	<div>房屋管理</div>
-</template>
-
-<style scoped></style>
--- .history/src/views/community/mall/index_20230525162021.vue
+++
@@ -1,7 +0,0 @@
-<script setup lang="ts"></script>
-
-<template>
-	<div>商铺管理</div>
-</template>
-
-<style scoped></style>
